129563105a78f08ebc36fd4b4cb7d16b6f40d9ff
Hans Christian Reinl <info@drublic.de>
2020-03-16 08:33:18 +0100

    Update modules, try to fix build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
+  mode: "development",
   entry: {
     Templates: [
       path.resolve('./Templates.js'),
@@ -10,16 +11,18 @@ module.exports = {
   cache: false,
   output: {
     path: path.resolve('dist/'),
-    filename: '[name].js'
+    filename: '[name].js',
+    library: '[name]',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        use: [{
-          loader: 'babel-loader',
-        }]
+        use: {
+          loader: "babel-loader"
+        }
       },
     ]
   }
